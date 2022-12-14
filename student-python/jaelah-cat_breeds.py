e1332cdfb267291b3250b90628b0353253fe4420
after3gamedesign <110561244+after3gamedesign@users.noreply.github.com>
2022-12-14 16:18:49 -0500

    Create jaelah-cat_breeds.py

---
+++ student-python/jaelah-cat_breeds.py
@@ -0,0 +1,25 @@
+cat_breeds=["persian", "siamese", "exotic", 'bombay', 'devon rex']       
+
+def breed_adder():
+ user_input= input("cat breed ")
+
+ cat_breeds.append(user_input)
+
+ print(cat_breeds)
+
+
+while True:
+  breed_adder()
+
+
+
+
+
+
+
+
+
+
+
+
+
