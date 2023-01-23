3960b7d649f27fc41f3131ec84683c9008583772
MDN <mark.denardo@gmail.com>
2023-01-23 16:16:30 -0500

    Create lesson-00.py

---
+++ python/lesson-00.py
@@ -0,0 +1,30 @@
+#  welcome to your first python lesson
+#  these first lines of text are behind comments
+#  to comment out a line of code use the '#'
+
+#comment out this text
+#print("uncomment this text and press Run")
+
+#  Run compiles the Python code
+#print('uncomment this text and press Run again.')
+
+#  print is the first function we'll learn in python. instructions: uncomment and run each line of code in these examples.
+
+#print(1+1)
+#print('This '+'sentence '+'is '+ 'concatenated.')
+
+# the print() function prints the data inside  parenthese; this area is where arguments are put;
+# an argument is any variable needed for a function to pass
+
+#  string data is being passed inside of this print function
+#print('this is data type called string')
+
+#  this is data type called a number
+#number = 10
+#print(number)
+
+#  there are also data types that deal in true or false called boolean
+#boo = True
+#print(boo) 
+
+
