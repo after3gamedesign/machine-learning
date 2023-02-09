2bd9a8544caa742a086d1b3611e3ea29207ac105
MDN <mark.denardo@gmail.com>
2023-02-08 20:21:11 -0500

    Create lesson_01_functions.py

---
+++ python/lesson_01_functions.py
@@ -0,0 +1,22 @@
+print("name")
+
+name = "warren"
+
+def your_name(name):
+  print(name)
+
+your_name(name)
+your_name("jaelah")
+
+name = input('Please enter your name: ')
+print("hi ",name)
+
+a=100
+b=10
+
+def add(a,b):
+  print("the sum is ", a+b)
+
+add(a,b)
+add(10,20)
+
