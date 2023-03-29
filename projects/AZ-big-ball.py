c1b677b2f8b903edacb97cc757c54240a79893cf
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-03-29 16:01:42 -0400

    Create AZ-big-ball.py

---
+++ projects/AZ-big-ball.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+for i in range(200):
+  t.circle(90)
+for i in range(110): 
+      col=("red")
+      t.circle(90)
+      t.forward(10)
+      t.right(30009)
