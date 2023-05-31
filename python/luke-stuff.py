f0777798bc37cd39700fa97930346aff543d49ba
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-05-31 16:13:39 -0400

    Create luke-stuff.py

---
+++ python/luke-stuff.py
@@ -0,0 +1,22 @@
+import turtle
+t = turtle.Turtle()
+#john cena irl
+t.speed(0)
+t.circle(90)
+t.right(90)
+t.forward(80)
+t.left(90)
+t.forward(200)
+t.right(180)
+t.forward(400)
+t.right(180)
+t.forward(200)
+t.right(135)
+t.forward(190)
+t.right(180)
+t.forward(190)
+t.right(90)
+t.forward(190)
+t.right(180)
+t.forward(190)
+
